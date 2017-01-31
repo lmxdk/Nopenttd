201e1a4403c56e0135e6e358604255fc2d96be7f
Lau Maack-Krommes <lmk@lmx.dk>
2017-01-31 21:33:44 +0100

    Group type

---
+++ nopenttd/src/group_type.cs
@@ -0,0 +1,55 @@
+/* $Id$ */
+
+/*
+ * This file is part of OpenTTD.
+ * OpenTTD is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+ * OpenTTD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file group_type.h Types of a group. */
+
+namespace Nopenttd
+{
+    /// Type for all group identifiers.
+    public struct GroupID
+    {
+        public ushort Id { get; set; }
+
+        public GroupID(ushort id)
+        {
+            Id = id;
+        }
+
+        public static implicit operator ushort(GroupID id)
+        {
+            return id.Id;
+        }
+
+        public static implicit operator GroupID(ushort id)
+        {
+            return new GroupID(id);
+        }
+    }
+
+    public class GroupConstants
+    {
+
+        public static readonly GroupID NEW_GROUP = 0xFFFC;
+
+        /// Sentinel for a to-be-created group.
+        public static readonly GroupID ALL_GROUP = 0xFFFD;
+
+        /// All vehicles are in this group.
+        public static readonly GroupID DEFAULT_GROUP = 0xFFFE;
+
+        /// Ungrouped vehicles are in this group.
+        public static readonly GroupID INVALID_GROUP = 0xFFFF;
+
+        /// Sentinel for invalid groups.
+
+        public static readonly uint MAX_LENGTH_GROUP_NAME_CHARS = 32; /// The maximum length of a group name in characters including '\0'
+    }
+
+    //struct Group;
+}
